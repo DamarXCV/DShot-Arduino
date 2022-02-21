a3bc2bb82a23b854a71191df7febc11b3e1f0afb
Damar <nickwedermann@web.de>
2022-02-21 19:07:34 +0100

    add telemetry; only use DSHOT600

--- src/DShot.h
+++ src/DShot.h
@@ -8,13 +8,6 @@
 #define DSHOT_PORT PORTD
 #endif
 
-#if defined(__AVR_ATmega8__)
-// For UNO, PortD 0-7: i.e. D0-D7
-#define DSHOT_PORT PORTD
-// ADDON for timers
-#define TIMSK1 TIMSK
-#endif
-
 #if defined(__AVR_ATmega32U4__)
 // For Leonardo, PortB 4-7: i.e. D8-D11
 #define DSHOT_PORT PORTB
@@ -22,17 +15,13 @@
 
 class DShot{
   public:
-    enum Mode {
-      DSHOT600,
-      DSHOT300,
-      DSHOT150
-    };
-    DShot(const enum Mode mode);
+    DShot();
     void attach(uint8_t pin);
-    uint16_t setThrottle(uint16_t throttle);
+    uint16_t setThrottle(uint16_t throttle, bool telemetry = false);
   private:
     uint16_t _packet = 0;
     uint16_t _throttle = 0;
+    bool _telemetry = false;
     uint8_t _pinMask = 0;
 };
 
